7f9077cc837a9248b149e03a2ca46dad62772a52
Ankit2533 <singhankit1833@gmail.com>
2024-05-30 15:04:46 +0530

    whatsapp updated

--- src/app/components/Whatsapp.tsx
+++ src/app/components/Whatsapp.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Whatsapp = () => {
   return (
     <div className="Whatsapp"> {/* Use the CSS module class */}
-      <a href="https://wa.me/917666655513" target="_blank" rel="noopener noreferrer"> {/* Add rel="noopener noreferrer" for security */}
+      <a href="https://wa.me/919322404922" target="_blank" rel="noopener noreferrer"> {/* Add rel="noopener noreferrer" for security */}
         <Image  src="/images/whatsapp.gif" alt="whatsapp"  width={100} height= {100}/> 
         </a>
     </div>
