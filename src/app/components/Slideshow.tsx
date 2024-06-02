ce54bbdf91d1a3661ebcb1746011642c28edd031
Ankit2533 <singhankit1833@gmail.com>
2024-06-03 02:39:56 +0530

    done

--- src/app/components/Slideshow.tsx
+++ src/app/components/Slideshow.tsx
@@ -2,43 +2,123 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from "next/image";
-import '/styles/style.css';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-const slideshow = () => {
+const Slideshow = () => {
   return (
-    <Swiper
-      slidesPerView={1}
-      spaceBetween={30}
-      loop={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={false}
-      modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper"
-    >
-      <SwiperSlide><Image src="/images/Events/1.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-      <SwiperSlide><Image src="/images/Events/2.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-      <SwiperSlide><Image src="/images/Events/3.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-      <SwiperSlide><Image src="/images/Events/4.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-      <SwiperSlide><Image src="/images/Events/5.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-      <SwiperSlide><Image src="/images/Events/6.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-      <SwiperSlide><Image src="/images/Events/7.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-      <SwiperSlide><Image src="/images/Events/8.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-      <SwiperSlide><Image src="/images/Events/9.jpg" alt="" width={1000} height={1000} layout="responsive" /></SwiperSlide>
-    </Swiper>
-  )
+    <div className="w-full h-full">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={false}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/1.jpg" 
+            alt="Event 1" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/2.jpg" 
+            alt="Event 2" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/3.jpg" 
+            alt="Event 3" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/4.jpg" 
+            alt="Event 4" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/5.jpg" 
+            alt="Event 5" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/6.jpg" 
+            alt="Event 6" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/7.jpg" 
+            alt="Event 7" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/8.jpg" 
+            alt="Event 8" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-center items-center w-full h-full">
+          <Image 
+            src="/images/Events/9.jpg" 
+            alt="Event 9" 
+            layout="responsive" 
+            width={1000} 
+            height={1000} 
+            className="object-cover w-full h-full" 
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
-export default slideshow;
+export default Slideshow;
