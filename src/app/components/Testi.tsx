a959d44e13a510a4cae6af760fda94b82aa0562d
Ankit2533 <singhankit1833@gmail.com>
2024-06-03 17:37:02 +0530

    background added for testimonials

--- src/app/components/Testi.tsx
+++ src/app/components/Testi.tsx
@@ -28,21 +28,31 @@ const testimonials = [
 
 const Testimonials: React.FC = () => {
   return (
-    <div className="w-full max-w-6xl mx-auto p-6">
-      <h2 className="text-6xl sm:text-7xl md:text-7xl lg:text-8xl xl:text-9xl font-medium title-font mb-4 golden-text text-center">What They Say</h2>
+    <div 
+      className="w-full h-full p-6 bg-cover bg-center bg-no-repeat" 
+      style={{ 
+        backgroundImage: 'url(/background/testibackground.jpg)', 
+        minHeight: '100vh', 
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed'
+      }}
+    >
+      <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-medium title-font mb-4 text-center golden-text">
+        What They Say
+      </h2>
       <Swiper
         modules={[Autoplay]}
         spaceBetween={50}
         slidesPerView={1}
         loop={true}
         autoplay={{ delay: 4490 }}
-        style={{ height: 'auto' }}
+        style={{ height: 'auto', background: 'transparent' }}
       >
         {testimonials.map((testimonial, index) => (
           <SwiperSlide key={index}>
-            <div className="flex flex-col items-center h-full pt-10 pb-10">
+            <div className="flex flex-col items-center h-full pt-10 pb-10 p-4">
               <div className="w-full h-auto flex-shrink-0 flex justify-center">
-                <div className="relative items-center" style={{ width: '80%', paddingBottom: '35.25%' }}>
+                <div className="relative" style={{ width: '80%', paddingBottom: '35.25%' }}>
                   <Image
                     src={testimonial.gif}
                     alt={`Testimonial ${index + 1}`}
@@ -52,11 +62,11 @@ const Testimonials: React.FC = () => {
                   />
                 </div>
               </div>
-              <div className="w-full px-10 py-4 flex-grow flex items-center justify-center">
-                <p className="text-lg sm:text-xl md:text-2xl text-center">
-                  <FaQuoteLeft className="inline-block mr-2 text-gray-500" />
+              <div className="w-full px-10 py-4 flex-grow flex items-center justify-center bg-transparent">
+                <p className="text-lg sm:text-xl md:text-2xl text-center text-black">
+                  <FaQuoteLeft className="inline-block mr-2 text-gray-300" />
                   {testimonial.text}
-                  <FaQuoteRight className="inline-block ml-2 text-gray-500" />
+                  <FaQuoteRight className="inline-block ml-2 text-gray-300" />
                 </p>
               </div>
             </div>
