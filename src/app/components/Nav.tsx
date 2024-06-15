c86da2603e38124d5237f3aa92107c81dff4256b
Ankit2533 <singhankit1833@gmail.com>
2024-06-15 17:58:44 +0530

    nav update

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,5 +1,5 @@
 "use client";
-import '/styles/font.css';
+import "/styles/font.css";
 import Image from "next/legacy/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
@@ -23,12 +23,16 @@ export const Navbar = () => {
     <>
       <nav className="flex items-center flex-wrap bg-[#091933] p-1 pl-15 pr-15 border-b-4 border-[#F7CF4F]">
         <Link href="/" className="inline-flex items-center p-2 mr-4">
-          <Image src="/images/logo.png" alt="" className="w-20" width={100} height={100} />
-          <span className="text-lg md:text-xl lg:text-2xl text-white font-bold tracking-wide para-text">
-  MMK Entertainment
-</span>
-
-
+          <Image
+            src="/images/logo.png"
+            alt=""
+            className="w-12 md:w-20"
+            width={80}
+            height={80}
+          />
+          <span className="text-lg md:text-xl lg:text-2xl text-white font-bold tracking-wide para-text ml-2 md:ml-4">
+            MMK Entertainment
+          </span>
         </Link>
         <button
           className="inline-flex p-3 hover:bg-[#F7CF4F] rounded lg:hidden text-white ml-auto hover:text-[#353534] outline-none"
@@ -36,21 +40,40 @@ export const Navbar = () => {
         >
           {active ? <FiX size={25} /> : <FiMenu size={25} />}
         </button>
-        <div className={`${active ? "" : "hidden"} w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
-          <div className="text-lg	 lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto para-text-bold">
-            <Link href="/" className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]">
+        <div
+          className={`${
+            active ? "" : "hidden"
+          } w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className="text-lg lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto para-text-bold">
+            <Link
+              href="/"
+              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]"
+            >
               HOME
             </Link>
-            <Link href="/about" className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]">
+            <Link
+              href="/about"
+              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]"
+            >
               ABOUT US
             </Link>
-            <Link href="/services" className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]">
+            <Link
+              href="/services"
+              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]"
+            >
               SERVICES
             </Link>
-            <Link href="/gallery" className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]">
+            <Link
+              href="/gallery"
+              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]"
+            >
               GALLERY
             </Link>
-            <Link href="/contact" className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]">
+            <Link
+              href="/contact"
+              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-[#F7CF4F] hover:text-[#353534]"
+            >
               CONTACT US
             </Link>
           </div>
